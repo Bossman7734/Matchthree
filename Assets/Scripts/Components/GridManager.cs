a9d77fc05b9720e415832d38e100966dc2af6bfd
Bahadır Osman UZUNTEPE <bhdrosman@gmail.com>
2024-07-19 16:57:33 +0300

    hint  tween converted to sequance and hint sequance stop on input

--- Assets/Scripts/Components/GridManager.cs
+++ Assets/Scripts/Components/GridManager.cs
@@ -48,6 +48,7 @@ namespace Components
         [OdinSerialize]private List<List<Tile>> _lastMatches;
         private Tile _hintTile;
         private GridDir _hintDir;
+        private Sequence _hintTween;
 
 
         private void Awake()
@@ -421,7 +422,7 @@ namespace Components
 
                 Vector3 moveCoords = _grid.CoordsToWorld(_transform, _hintTile.Coords + gridMoveDir);
 
-                _hintTile.DOMove(moveCoords);
+               _hintTween = _hintTile.DOHint(moveCoords);
             }
         }
 
@@ -441,6 +442,10 @@ namespace Components
         {
             _selectedTile = clickedTile;
             _mouseDownPos = dirVector;
+            if (_hintTween.IsActive())
+            {
+                _hintTween.Complete();
+            }
         }
 
         private void OnMouseUpGrid(Vector3 mouseUpPos)
