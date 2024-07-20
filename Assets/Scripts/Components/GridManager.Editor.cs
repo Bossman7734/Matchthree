a5638ba4c80adefa32ca7e3cfffff27b6488c2a6
Bahadır Osman UZUNTEPE <bhdrosman@gmail.com>
2024-07-20 17:59:49 +0300

    Sprite layer orders+grid spawn masking

--- Assets/Scripts/Components/GridManager.Editor.cs
+++ Assets/Scripts/Components/GridManager.Editor.cs
@@ -74,6 +74,13 @@ namespace Components
                 
                 _gridBounds.Encapsulate(spriteBounds);
             }
+
+            foreach (GameObject border in  _gridBorders)
+            {
+                Bounds spriteBounds = border.GetComponentInChildren<SpriteRenderer>().bounds;
+                
+                _gridBounds.Encapsulate(spriteBounds);
+            }
         }
         
 
@@ -125,6 +132,7 @@ namespace Components
                 
             }
             GenerateTileBG();
+            GenerateBorders();
             CalculateBounds();
         }
 
@@ -133,6 +141,7 @@ namespace Components
         {
 
             _tileBGs.DoToAll(DestroyImmediate);
+            _tileBGs = new List<GameObject>();
             
             foreach (Tile tile in _grid)
             {
@@ -150,6 +159,52 @@ namespace Components
             }
         }
         
+        [Button]
+        private void GenerateBorders()
+        {
+            _gridBorders.DoToAll(DestroyImmediate);
+            _gridBorders = new List<GameObject>();
+            
+            
+            Tile botLeftCorner = _grid[0,0];
+            InstantiateBorder(botLeftCorner.transform.position, _borderBotLeft);
+            Tile topRightCorner = _grid[_grid.GetLength(0) - 1, _grid.GetLength(1) - 1];
+            InstantiateBorder(topRightCorner.transform.position, _borderTopRight);
+            Tile botRightCorner = _grid[_grid.GetLength(0) - 1,0];
+            InstantiateBorder(botRightCorner.transform.position, _borderBotRıght);
+            Tile topLeftCorner = _grid[0,_grid.GetLength(1) - 1];
+            InstantiateBorder(topLeftCorner.transform.position, _borderTopLeft);
+
+
+            for (int x = 0; x < _grid.GetLength(0); x++) 
+            {
+                Tile tileBot = _grid[x, 0];
+                Tile tileTop = _grid[x,_grid.GetLength(1) -1];
+
+                InstantiateBorder(tileBot.transform.position, _borderBot);
+                InstantiateBorder(tileTop.transform.position, _borderTop);
+            }
+            for (int y = 0; y < _grid.GetLength(1); y++)
+            {
+                Tile tileLeft = _grid[0, y];
+                Tile tileRıght = _grid[_grid.GetLength(0) - 1, y];
+                
+                InstantiateBorder(tileLeft.transform.position, _borderLeft);
+                InstantiateBorder(tileRıght.transform.position, _borderRight);
+            }
+        }
+
+        private void InstantiateBorder(Vector3 tileWPos, GameObject borderPrefab)
+        {
+            GameObject newBorder = Instantiate
+            (
+                borderPrefab,
+                tileWPos,
+                quaternion.identity,
+                _borderTrans
+            );
+            _gridBorders.Add(newBorder);
+        }
 #endif       
     }
 }
